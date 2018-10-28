67d3b749d39b1d755135545efa122098da986a86
Ryan Baron <ryan@madtownagency.com>
2018-10-28 13:02:47 -0700

    udpating api.js to newtworkNode.js and using port variable instead of hard coded port of '3000'

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -2,6 +2,7 @@ const express = require( 'express' );
 const bodyParser = require( 'body-parser' );
 const uuid = require( 'uuid/v1');
 
+const port = process.argv[2];
 const Blockchain = require( './blockchain' );
 const configBlockchain = require( './config.blockchain' );
 
@@ -83,6 +84,6 @@ app.get('/mine', function( req, res ) {
 
 } );
 
-app.listen( 3000, function(){
-  console.log( 'Listening on port 3000...' );
+app.listen( port, function(){
+  console.log( `Listening on port ${port}.` );
 } );
