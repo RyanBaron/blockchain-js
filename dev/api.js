88b53a28eeb9b160cb758f6f1c8ae52bae539700
Ryan Baron <ryan@madtownagency.com>
2018-10-28 12:07:31 -0700

    adding test transaction functionality

--- dev/api.js
+++ dev/api.js
@@ -17,6 +17,12 @@ app.get('/blockchain', function( req, res ) {
 
 app.post('/transaction', function( req, res ) {
 
+  // Create a new transaction with the request data.
+  const blockIndex = blockchain.createNewTransaction( req.body.amount, req.body.sender, req.body.recipient );
+
+  // Return the index of the block the transaction is added to.
+  return res.json( { status: true, msg: `Transaction will be added in block: ${blockIndex}.` } );
+
 } );
 
 app.get('/mine', function( req, res ) {
