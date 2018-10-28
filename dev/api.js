c10c0973ed1af44d71470eaf65299e5979c6df7a
Ryan Baron <ryan@madtownagency.com>
2018-10-28 11:24:01 -0700

    adding 3 empty endpoints: blockchain, transaction and mine

--- dev/api.js
+++ dev/api.js
@@ -1,8 +1,18 @@
 let express = require('express');
 let app = express();
 
-app.get('/', function( req, res ) {
-  res.send('Hello Blockchain')
+app.get('/blockchain', function( req, res ) {
+
 } );
 
-app.listen( 3000 );
+app.post('/transation', function( req, res ) {
+
+} );
+
+app.get('/mine', function( req, res ) {
+
+} );
+
+app.listen( 3000, function(){
+  console.log('Listening on port 3000...');
+} );
