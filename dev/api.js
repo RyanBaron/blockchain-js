17fdd798c2ecc29f2bea37c2235a12c5c0a4a3cf
Ryan Baron <ryan@madtownagency.com>
2018-10-28 11:55:15 -0700

    using body-parser package

--- dev/api.js
+++ dev/api.js
@@ -1,11 +1,16 @@
-let express = require('express');
-let app = express();
+const express = require( 'express' );
+const bodyParser = require( 'body-parser' );
+
+const app = express();
+
+app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded( { extended: false } ) ) ;
 
 app.get('/blockchain', function( req, res ) {
 
 } );
 
-app.post('/transation', function( req, res ) {
+app.post('/transaction', function( req, res ) {
 
 } );
 
@@ -14,5 +19,5 @@ app.get('/mine', function( req, res ) {
 } );
 
 app.listen( 3000, function(){
-  console.log('Listening on port 3000...');
+  console.log( 'Listening on port 3000...' );
 } );
